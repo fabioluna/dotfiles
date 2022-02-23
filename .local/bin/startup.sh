a48ba2f4eae428f3a51ea9a680acb5644e22c2a7
Fábio Luna <fabiocluna@hotmail.com>
2022-02-23 16:13:24 -0300

    Remove unnecessary scripts from startup

--- .local/bin/startup.sh
+++ .local/bin/startup.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
-#/home/fabio/./preroute.sh
-
-# Change max user watches
-# echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
-
-# Start Services
-# service postgresql start
-# service mysql start
-#/etc/init.d/docker start
-#sleep 2
-#service nginx start
-
-# Start Docker Containers 
-# docker start pgadmin
-# docker start myadmin
 # Postgres
 cd /home/fabio/.docker/postgres
 docker-compose up -d
 
+# Nginx
 cd /home/fabio/.docker/nginx
 docker-compose up -d
