2a49a2efe113e3b3956489f916e0d6ba6c227cce
Fábio Luna <fabiocluna@proton.me>
2024-11-04 16:37:36 -0300

    🎨 Change initial Facer Config

--- home/.config/qtile/autostart.sh
+++ home/.config/qtile/autostart.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 dex -a
-facer_rgb -m 3
-
+# facer_rgb -m 3
+facer_rgb -m 0 -z 1 -cR 160 -cB 32 -cG 5
+facer_rgb -m 0 -z 2 -cR 160 -cB 32 -cG 5
+facer_rgb -m 0 -z 3 -cR 160 -cB 32 -cG 5
